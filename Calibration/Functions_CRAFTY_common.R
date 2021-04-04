b8cca494d458c7548938311a3d32ad4877af0ccb
alanbseo <pompoco78@gmail.com>
2021-04-04 04:26:10 +0200

    Clean and leave only the source code

--- Calibration/Functions_CRAFTY_common.R
+++
@@ -1,9 +0,0 @@
-
-# Cell ID and cooridnates 
-ctry.ids <- read.csv("~/Dropbox/KIT/CLIMSAVE/IAP/Cell_ID_LatLong.csv")
-x.lat.v = sort(unique(ctry.ids$Longitude))
-y.lon.v = sort(unique(ctry.ids$Latitude))
-
-
-# Lon-Lat projection 
-proj4.LL <- CRS("+proj=longlat +datum=WGS84")
